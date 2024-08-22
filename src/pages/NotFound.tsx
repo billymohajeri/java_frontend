a32f960f7484077d3d45662108ec778318f78022
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-08-23 02:31:23 +0300

    Update not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,34 +1,35 @@
-import { useNavigate, useRouteError } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
+import { useNavigate, useRouteError } from "react-router-dom"
 
 const NotFound = () => {
   const error: any = useRouteError()
-
   const navigate = useNavigate()
 
   return (
-    <div className="container mt-5">
-      <div className="card text-center">
-        <div className="card-body">
-          <h1 className="card-title">Oops!</h1>
-          <p className="card-text">Sorry, an unexpected error has occurred.</p>
+    <>
+      <div className="flex items-center justify-center h-screen ">
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg p-5 max-w-lg w-full text-center">
+          <h1 className="text-2xl font-bold text-gray-900">Oops!</h1>
+          <p className="mt-3 text-base text-gray-500">Sorry, an unexpected error has occurred.</p>
           <img
             src="../src/assets/404.jpg"
-            className="img-fluid mb-4 not-found-image"
+            className="mx-auto mt-4 w-1/2 h-auto rounded"
             alt="404 Not Found"
           />
           {error && (
-            <p className="card-text">
-              <i>{error.statusText || error.message}</i>
-            </p>
+            <div>
+              <p className="mt-3 italic text-sm text-red-500">
+                {error.statusText || error.message}
+              </p>
+              <br />
+            </div>
           )}
-          <div className="text-center mt-4">
-            <button className="btn btn-primary" onClick={() => navigate('/')}>
-              Back to Home
-            </button>
-          </div>
+          <Button variant="secondary" onClick={() => navigate("/")}>
+            Back to Home
+          </Button>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
