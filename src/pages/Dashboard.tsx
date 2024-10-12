b001cfe12899f9091a0869aa6d6f8e1fca1387c3
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-10-13 01:17:43 +0300

    Update show error in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -86,166 +86,168 @@ const Dashboard = () => {
 
   return (
     <>
-      {(!token || role === "USER") && <NoAccess />}
-
-      <>
-        <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-24">
-          <Card>
-            <CardHeader>
-              <CardTitle>Total Products</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <h4 className="text-2xl font-bold">
-                {isLoadingProducts ? <SmallLoading /> : products?.length}
-              </h4>
-              {isErrorProducts && <p>No orders to show</p>}
-            </CardContent>
-            {products && (
-              <CardFooter className="flex justify-start mt-auto">
-                <Button onClick={() => navigate("/products")}>
-                  <ShoppingBag className="mr-4" />
-                  See All
-                </Button>
-              </CardFooter>
-            )}
-          </Card>
-          <Card className="flex flex-col h-full">
-            <div className="flex items-start">
-              <CardHeader className="flex-1">
-                <CardTitle>
-                  <div>
-                    Total Users:
-                    {isLoadingUsers ? (
-                      <SmallLoading />
-                    ) : (
-                      users && <h4 className="text-2xl font-bold">{users.length}</h4>
-                    )}
-                  </div>
-                </CardTitle>
-              </CardHeader>
-              <div className="flex flex-col p-5 ">
-                <div className="text-lg flex">
-                  <UserCheck className="ml-1" />
-                  <p className="ml-4">Admins: {adminCount}</p>
-                </div>
-                <div className="text-lg flex">
-                  <User2Icon />
-                  <p className="ml-5">Users: {userCount}</p>
-                </div>
-              </div>
-            </div>
-            <CardContent>{isErrorUsers && <p className="mt-4">No users to show</p>}</CardContent>
-            {users && (
-              <CardFooter className="flex justify-start mt-auto">
-                <Button onClick={() => navigate("/users")}>
-                  <Users2Icon className="mr-4" />
-                  See All
-                </Button>
-              </CardFooter>
-            )}
-          </Card>
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Total Orders</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {isLoadingOrders ? (
-                <SmallLoading />
-              ) : orders ? (
-                <h4 className="text-2xl font-bold">${orders.length}</h4>
-              ) : (
-                <p>No orders to show</p>
-              )}
-            </CardContent>
-
-            {orders && (
-              <CardFooter className="flex justify-start mt-auto">
-                <Button onClick={() => navigate("/orders")}>
-                  <ShoppingCart className="mr-4" />
-                  See All
-                </Button>
-              </CardFooter>
-            )}
-          </Card>
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Total Sales</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {isLoadingPayments ? (
-                <SmallLoading />
-              ) : payments ? (
-                <h4 className="text-2xl font-bold">
-                  ${payments.reduce((total, payment) => total + payment.amount, 0).toFixed(2)}
-                </h4>
-              ) : (
-                <p>No sales to show</p>
-              )}
-            </CardContent>
-            {payments && (
-              <CardFooter className="flex justify-start mt-auto">
-                <Button onClick={() => navigate("/payments")}>
-                  <EuroIcon className="mr-4" />
-                  See All
-                </Button>
-              </CardFooter>
-            )}
-          </Card>
-        </div>
-        <div className="grid grid-cols-3 gap-6 p-6">
-          <div className="col-span-2">
+      {!token || role === "USER" ? (
+        <NoAccess />
+      ) : (
+        <div>
+          <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-24">
             <Card>
               <CardHeader>
-                <CardTitle>Recent Orders</CardTitle>
+                <CardTitle>Total Products</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <h4 className="text-2xl font-bold">
+                  {isLoadingProducts ? <SmallLoading /> : products?.length}
+                </h4>
+                {isErrorProducts && <p>No orders to show</p>}
+              </CardContent>
+              {products && (
+                <CardFooter className="flex justify-start mt-auto">
+                  <Button onClick={() => navigate("/products")}>
+                    <ShoppingBag className="mr-4" />
+                    See All
+                  </Button>
+                </CardFooter>
+              )}
+            </Card>
+            <Card className="flex flex-col h-full">
+              <div className="flex items-start">
+                <CardHeader className="flex-1">
+                  <CardTitle>
+                    <div>
+                      Total Users:
+                      {isLoadingUsers ? (
+                        <SmallLoading />
+                      ) : (
+                        users && <h4 className="text-2xl font-bold">{users.length}</h4>
+                      )}
+                    </div>
+                  </CardTitle>
+                </CardHeader>
+                <div className="flex flex-col p-5 ">
+                  <div className="text-lg flex">
+                    <UserCheck className="ml-1" />
+                    <p className="ml-4">Admins: {adminCount}</p>
+                  </div>
+                  <div className="text-lg flex">
+                    <User2Icon />
+                    <p className="ml-5">Users: {userCount}</p>
+                  </div>
+                </div>
+              </div>
+              <CardContent>{isErrorUsers && <p className="mt-4">No users to show</p>}</CardContent>
+              {users && (
+                <CardFooter className="flex justify-start mt-auto">
+                  <Button onClick={() => navigate("/users")}>
+                    <Users2Icon className="mr-4" />
+                    See All
+                  </Button>
+                </CardFooter>
+              )}
+            </Card>
+
+            <Card>
+              <CardHeader>
+                <CardTitle>Total Orders</CardTitle>
               </CardHeader>
               <CardContent>
                 {isLoadingOrders ? (
                   <SmallLoading />
                 ) : orders ? (
-                  <ul>
-                    {orders.slice(0, 7).map((order) => (
-                      <li key={order.id} className="border-b py-2">
-                        <Link to={`/orders/${order.id}`}>
-                          <span>{order.address}</span> - <span>{`${order.comments}`}</span> -{" "}
-                          <span>{`${order.status}`}</span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+                  <h4 className="text-2xl font-bold">${orders.length}</h4>
                 ) : (
                   <p>No orders to show</p>
                 )}
               </CardContent>
+
+              {orders && (
+                <CardFooter className="flex justify-start mt-auto">
+                  <Button onClick={() => navigate("/orders")}>
+                    <ShoppingCart className="mr-4" />
+                    See All
+                  </Button>
+                </CardFooter>
+              )}
             </Card>
-          </div>
-          <div className="">
+
             <Card>
               <CardHeader>
-                <CardTitle>Recent Payments</CardTitle>
+                <CardTitle>Total Sales</CardTitle>
               </CardHeader>
               <CardContent>
                 {isLoadingPayments ? (
                   <SmallLoading />
                 ) : payments ? (
-                  <ul>
-                    {payments.slice(0, 7).map((payment) => (
-                      <li key={payment.id} className="border-b py-2">
-                        <Link to={`/payments/${payment.id}`}>
-                          <span>€ {payment.amount}</span> - <span>{`$${payment.status}`}</span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+                  <h4 className="text-2xl font-bold">
+                    ${payments.reduce((total, payment) => total + payment.amount, 0).toFixed(2)}
+                  </h4>
                 ) : (
-                  <p>No payments to show</p>
+                  <p>No sales to show</p>
                 )}
               </CardContent>
+              {payments && (
+                <CardFooter className="flex justify-start mt-auto">
+                  <Button onClick={() => navigate("/payments")}>
+                    <EuroIcon className="mr-4" />
+                    See All
+                  </Button>
+                </CardFooter>
+              )}
             </Card>
           </div>
+          <div className="grid grid-cols-3 gap-6 p-6">
+            <div className="col-span-2">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Recent Orders</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {isLoadingOrders ? (
+                    <SmallLoading />
+                  ) : orders ? (
+                    <ul>
+                      {orders.slice(0, 7).map((order) => (
+                        <li key={order.id} className="border-b py-2">
+                          <Link to={`/orders/${order.id}`}>
+                            <p>{order.address}</p>
+                            <span>{`${order.comments}`}</span> - <span>{`${order.status}`}</span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>No orders to show</p>
+                  )}
+                </CardContent>
+              </Card>
+            </div>
+            <div className="">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Recent Payments</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {isLoadingPayments ? (
+                    <SmallLoading />
+                  ) : payments ? (
+                    <ul>
+                      {payments.slice(0, 11).map((payment) => (
+                        <li key={payment.id} className="border-b py-2">
+                          <Link to={`/payments/${payment.id}`}>
+                            <span>€ {payment.amount}</span> - <span>{`${payment.status}`}</span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>No payments to show</p>
+                  )}
+                </CardContent>
+              </Card>
+            </div>
+          </div>
         </div>
-      </>
+      )}
     </>
   )
 }
--- src/pages/OrderList.tsx
+++ src/pages/OrderList.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 import { useQuery, useQueryClient } from "@tanstack/react-query"
 
 import api from "@/api"
-import {Loading} from "@/components/Loading"
+import { Loading } from "@/components/Loading"
 import NoAccess from "@/components/NoAccess"
 import { orderSchema } from "@/schemas/order"
 import ShowError from "@/components/ShowError"
@@ -51,61 +51,61 @@ const OrderList = () => {
     Delivered = "DELIVERED",
     Canceled = "CANCELED"
   }
-  const [userId, setUserId] = useState("")
+  // const [userId, setUserId] = useState("")
   // const [dateTime, setDateTime] = useState<number[]>([])
-  const [dateTime, setDateTime] = useState("")
-  const [comments, setComments] = useState("")
-  const [status, setStatus] = useState("")
-  const [address, setAddress] = useState("")
-  const [products, setProducts] = useState<Product[]>([])
-  const [validationErrors, setValidationErrors] = useState<ZodIssue[]>([])
-  const [open, setOpen] = useState(false)
-  const queryClient = useQueryClient()
+  // const [dateTime, setDateTime] = useState("")
+  // const [comments, setComments] = useState("")
+  // const [status, setStatus] = useState("")
+  // const [address, setAddress] = useState("")
+  // const [products, setProducts] = useState<Product[]>([])
+  // const [validationErrors, setValidationErrors] = useState<ZodIssue[]>([])
+  // const [open, setOpen] = useState(false)
+  // const queryClient = useQueryClient()
 
-  const handleAddOrder = async (addOrder: Omit<Order, "id">) => {
-    const result = orderSchema.safeParse(addOrder)
-    if (!result.success) {
-      setValidationErrors(result.error.errors)
-    } else {
-      setValidationErrors([])
-      try {
-        const res = await api.post(`/orders`, addOrder)
-        if (res.status == 201) {
-          toast({
-            title: "✅ Added!",
-            className: "bg-neutral-300 text-black dark:bg-neutral-600 dark:text-white",
-            description: `Order added successfully.`
-          })
-          queryClient.invalidateQueries({ queryKey: ["orders"] })
-          setOpen(false)
-          return res.data.data
-        }
-      } catch (error: unknown) {
-        if (axios.isAxiosError(error)) {
-          const axiosError = error as AxiosError<ApiErrorResponse>
-          if (axiosError.response) {
-            toast({
-              title: "❌ Adding order failed!",
-              className: "bg-red-100 text-black",
-              description: `${axiosError.response.data.error.message}`
-            })
-          } else {
-            toast({
-              title: "❌ Adding order failed!",
-              className: "bg-red-100 text-black",
-              description: error.message || "An unknown error occurred."
-            })
-          }
-        } else {
-          toast({
-            title: "❌ Adding order failed!",
-            className: "bg-red-100 text-black",
-            description: "An unknown error occurred."
-          })
-        }
-      }
-    }
-  }
+  // const handleAddOrder = async (addOrder: Omit<Order, "id">) => {
+  //   const result = orderSchema.safeParse(addOrder)
+  //   if (!result.success) {
+  //     setValidationErrors(result.error.errors)
+  //   } else {
+  //     setValidationErrors([])
+  //     try {
+  //       const res = await api.post(`/orders`, addOrder)
+  //       if (res.status == 201) {
+  //         toast({
+  //           title: "✅ Added!",
+  //           className: "bg-neutral-300 text-black dark:bg-neutral-600 dark:text-white",
+  //           description: `Order added successfully.`
+  //         })
+  //         queryClient.invalidateQueries({ queryKey: ["orders"] })
+  //         setOpen(false)
+  //         return res.data.data
+  //       }
+  //     } catch (error: unknown) {
+  //       if (axios.isAxiosError(error)) {
+  //         const axiosError = error as AxiosError<ApiErrorResponse>
+  //         if (axiosError.response) {
+  //           toast({
+  //             title: "❌ Adding order failed!",
+  //             className: "bg-red-100 text-black",
+  //             description: `${axiosError.response.data.error.message}`
+  //           })
+  //         } else {
+  //           toast({
+  //             title: "❌ Adding order failed!",
+  //             className: "bg-red-100 text-black",
+  //             description: error.message || "An unknown error occurred."
+  //           })
+  //         }
+  //       } else {
+  //         toast({
+  //           title: "❌ Adding order failed!",
+  //           className: "bg-red-100 text-black",
+  //           description: "An unknown error occurred."
+  //         })
+  //       }
+  //     }
+  //   }
+  // }
 
   const navigate = useNavigate()
   const context = useContext(UserContext)
@@ -135,22 +135,22 @@ const OrderList = () => {
     enabled: context?.user?.role === "ADMIN"
   })
 
-  const errorsAsObject = validationErrors.reduce((validationErrors, validationError) => {
-    return {
-      ...validationErrors,
-      [validationError.path[0]]: validationError.message
-    }
-  }, {} as { [key: string]: string })
+  // const errorsAsObject = validationErrors.reduce((validationErrors, validationError) => {
+  //   return {
+  //     ...validationErrors,
+  //     [validationError.path[0]]: validationError.message
+  //   }
+  // }, {} as { [key: string]: string })
 
-  const handleReset = () => {
-    setUserId("")
-    setDateTime("")
-    setComments("")
-    setStatus("")
-    setAddress("")
-    setProducts([])
-    setValidationErrors([])
-  }
+  // const handleReset = () => {
+  //   setUserId("")
+  //   setDateTime("")
+  //   setComments("")
+  //   setStatus("")
+  //   setAddress("")
+  //   setProducts([])
+  //   setValidationErrors([])
+  // }
 
   return (
     <>
@@ -171,170 +171,13 @@ const OrderList = () => {
           <h4 className="text-center text-sm text-gray-400 mb-5">
             Click on each item to see details and more actions.
           </h4>
-          <Dialog open={open} onOpenChange={setOpen}>
-            <div className="grid items-center justify-center my-5">
-              <DialogTrigger asChild>
-                <Button onClick={handleReset}>
-                  <Plus className="mr-4" />
-                  Add Order
-                </Button>
-              </DialogTrigger>
-            </div>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Add Order</DialogTitle>
-                <DialogDescription>
-                  Enter the new Order&apos;s information here. Click Add when you&apos;re done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="userId" className="text-right">
-                    User ID *
-                  </Label>
-                  <Input
-                    id="userId"
-                    name="userId"
-                    value={userId}
-                    onChange={(e) => setUserId(e.target.value)}
-                    className="col-span-3"
-                  />
-                </div>
-                {errorsAsObject["userId"] && (
-                  <p className="text-red-400">{errorsAsObject["userId"]}</p>
-                )}
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="dateTime" className="text-right">
-                    Date & Time
-                  </Label>
-                  <Input
-                    id="dateTime"
-                    name="dateTime"
-                    value={dateTime}
-                    onChange={(e) => setDateTime(e.target.value)}
-                    className="col-span-3"
-                    type="text"
-                  />
-                </div>
-                {/* {errorsAsObject["dateTime"] && (
-              <p className="text-red-400">{errorsAsObject["dateTime"]}</p>
-            )} */}
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="comments" className="text-right">
-                    Comments
-                  </Label>
-                  <Input
-                    id="comments"
-                    name="comments"
-                    value={comments}
-                    onChange={(e) => setComments(e.target.value)}
-                    className="col-span-3"
-                  />
-                </div>
-
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="status" className="text-right">
-                    Status *
-                  </Label>
-                  <Select name="status" onValueChange={(value) => setStatus(value)}>
-                    <SelectTrigger className="col-span-3">
-                      <SelectValue placeholder="Set Status" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectGroup>
-                        <SelectItem value={OrderStatus.Pending}>Pending</SelectItem>
-                        <SelectItem value={OrderStatus.Shipped}>Shipped</SelectItem>
-                        <SelectItem value={OrderStatus.Delivered}>Delivered</SelectItem>
-                        <SelectItem value={OrderStatus.Canceled}>Canceled</SelectItem>
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                </div>
-                {errorsAsObject["status"] && (
-                  <p className="text-red-400">{errorsAsObject["status"]}</p>
-                )}
-                <div className="grid grid-cols-4 items-center gap-4">
-                  <Label htmlFor="address" className="text-right">
-                    Address *
-                  </Label>
-                  <Input
-                    id="address"
-                    name="address"
-                    value={address}
-                    onChange={(e) => setAddress(e.target.value)}
-                    className="col-span-3"
-                  />
-                </div>
-                {errorsAsObject["address"] && (
-                  <p className="text-red-400">{errorsAsObject["address"]}</p>
-                )}
-                {/* <div className="grid grid-cols-4 items-center gap-4">
-              <Label htmlFor="products" className="text-right">
-                Products *
-              </Label>
-              <Input
-                id="products"
-                name="products"
-                value={products}
-                onChange={(e) => setProducts([{"id":e.target.value}])}
-                className="col-span-3"
-              />
-            </div>
-          {errorsAsObject["products"] && (
-            <p className="text-red-400">{errorsAsObject["products"]}</p>
-          )} */}
-              </div>
-
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="button" variant="secondary">
-                    Cancel
-                  </Button>
-                </DialogClose>
-                <Button
-                  onClick={() => {
-                    handleAddOrder({
-                      userId,
-                      dateTime,
-                      comments,
-                      status,
-                      address,
-
-                      products: [
-                        {
-                          id: "dcaa3927-fc91-423b-8afa-ce3c200001b4",
-                          name: "Fun Pattern Crew Socks",
-                          price: 9.99,
-                          description:
-                            "1 Pair Of Men's Cotton Blend Fashion Novel Fun Pattern Crew Socks, Soft & Lightweight All-match Unisex Socks, For Gifts, Outdoor Wearing & All Seasons Wearing",
-                          images: [
-                            "https://img.kwcdn.com/product/fancy/c41cfa3a-a8bf-4bed-abbe-8d33289dde34.jpg?imageView2/2/w/800/q/70/format/webp"
-                          ],
-                          color: "Green",
-                          meta: "null",
-                          rating: 4.5,
-                          stock: 100
-                        }
-                      ]
-                    })
-                  }}
-                >
-                  Add
-                </Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-
           <Table className="">
             <TableHeader>
               <TableRow>
                 <TableHead>No.</TableHead>
                 <TableHead>ID</TableHead>
                 <TableHead>User ID</TableHead>
-                {/* <TableHead>Date & Time</TableHead> */}
-                {/* <TableHead>Comments</TableHead> */}
                 <TableHead>Status</TableHead>
-                {/* <TableHead>Address</TableHead> */}
               </TableRow>
             </TableHeader>
             <TableBody>
@@ -347,10 +190,7 @@ const OrderList = () => {
                   <TableCell>{index + 1}</TableCell>
                   <TableCell>{order.id}</TableCell>
                   <TableCell>{order.userId}</TableCell>
-                  {/* <TableCell>{order.dateTime.join(".")}</TableCell>
-              <TableCell>{order.comments}</TableCell> */}
                   <TableCell>{order.status}</TableCell>
-                  {/* <TableCell>{order.address}</TableCell> */}
                 </TableRow>
               ))}
             </TableBody>
